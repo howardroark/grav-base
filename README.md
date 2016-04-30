bde063c605d1f0959e66b7a8dac66bb72bd7bffa
Howard Roark <howie@codingcoop.org>
2016-04-30 15:15:57 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+Fork this repo to try it out yourself!
+
+### Dev
+
+```
+vagrant up
+```
+
+[http://localhost:8000/](http://localhost:8000)
+
+### Web
+
+[![Install on DigitalOcean](https://dobutton.club/button.svg)](https://dobutton.club/install?url=https://github.com/howardroark/gravops)
---
+++ ops/web/provision.sh
@@ -0,0 +1,24 @@
+apt-get update
+apt-get install -y git unzip tree vim nginx php5-fpm php5-curl php5-dev php5-cli php5-gd
+
+git clone https://github.com/howardroark/gravops.git /tmp/gravops
+cd /tmp/gravops/ops/web
+
+cp files/nginx.conf /etc/nginx/nginx.conf
+service nginx restart
+
+cp files/www.conf /etc/php5/fpm/pool.d/www.conf
+service php5-fpm restart
+
+if [ -d /grav-admin ] ; then
+    echo "Grav is already installed"
+else
+    curl -L https://github.com/getgrav/grav/releases/download/1.0.10/grav-admin-v1.0.10.zip -o /tmp/grav-admin.zip > /dev/null
+    if [ `md5sum /tmp/grav-admin.zip | awk '{ print $1 }'` != "6bb6250e7e3cce7c95a78df04478b767" ]
+        then exit 1
+    fi
+    unzip /tmp/grav-admin.zip -d /
+    chown -R gravops:gravops /grav-admin
+fi
+
+rm -rf /tmp/gravops
