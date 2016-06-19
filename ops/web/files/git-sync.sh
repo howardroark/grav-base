e7511887f994e55e592d7869d7978e26fb86e105
Howard Roark <howie@codingcoop.org>
2016-06-19 12:11:46 -0700

    Fixing script issue.

--- ops/web/files/git-sync.sh
+++ ops/web/files/git-sync.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #http://stackoverflow.com/a/3879077
 cd /project
-if [ git diff-index --quiet HEAD -- ]; then
+if ! git diff-index --quiet HEAD --; then
     git add .
     git commit -m "Grav Admin updates."
     git push origin master
